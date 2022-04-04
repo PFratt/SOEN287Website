fed26e9bc6ccbf2e5605cdea7c770854dd823602
im-samwong <98343790+im-samwong@users.noreply.github.com>
2022-04-04 11:19:47 -0400

    Package 2

--- data.js
+++ data.js
@@ -1,55 +1,20 @@
 var data_info; 
 var list_of_products = new Array();
-var targetElements = new Array();
 
 
 window.addEventListener('load', addEventListenerTop);
      
-/*function formActionToATags(){
-	var aTags = document.getElementsByTagName('a');
-	for(let i = 0; i < aTags.length; i++){
-		if(aTags[i].classList.contains("food-item") or aTags[i].classList.contains("food-name"))
-			targetElements.push(aTags[i]);
-	}
-	for(let i = 0; i < targetElements.length; i++)
-		targetElements[i].addEventListener('click',submitForm);
-}
 
-function submitForm(){
-
-}*/
-
-function aisleLocation(aisle) {
-	var aisle_location;
-	switch(aisle.toLowerCase()){
-		case "fruits & vegetables":
-				aisle_location = "fruits-vegetables.php";
-				break;
-		case "dairy & eggs":
-				aisle_location = "dairy-eggs.php";
-				break;
-		case "meat & poultry":
-				aisle_location = "meats-poultry.php";
-				break;
-		case "fish & seafood":
-				aisle_location = "fish-seafood.php";
-				break;
-		}
-	return aisle_location;
-}
 
 function topFunction() {
 	var navButton = document.getElementsByClassName("navbar-toggler float-right")[0];
-	var navBar = document.getElementsByClassName("navbar navbar-expand-md justify-content-center tertiary-color border-bottom row")[0];
-	var dimmer = $('.dimmer');
 	if(navButton.getAttribute('aria-expanded') == 'false'){
-		dimmer.show();
-	} else {
-		dimmer.hide();
+		document.body.scrollTop = 0;
+  		document.documentElement.scrollTop = 0;
 	}
 } 
 
-function product(name, id, origin, discount_price, regular_price, description, image_ref, aisle, aisle_location, numOfItems, quantity, popular) {
+function product(name, id, origin, discount_price, regular_price, description, image_ref, aisle, aisle_location, numOfItems, quantity) {
 	this.name = name;
 	this.id = id;
 	this.origin = origin;
@@ -61,12 +26,11 @@ function product(name, id, origin, discount_price, regular_price, description, i
 	this.numOfItems = numOfItems;
 	this.quantity = quantity;
 	this.aisle_location = aisle_location;
-	this.popular = popular;
 }
 
 
 
-/*function loadDoc() {
+function loadDoc() {
 	const xhttp = new XMLHttpRequest();
 	xhttp.open("GET","database.txt", true);
 	xhttp.onload = function() {
@@ -75,23 +39,23 @@ function product(name, id, origin, discount_price, regular_price, description, i
 		};
 	xhttp.send();
     	
-}*/
+}
 
-/*function toArray(data) {
+function toArray(data) {
 	for(let i = 0; i < data.length; i++)
 		{
 			var temp = new product(data[i].name, i, data[i].origin, data[i].discount_price, data[i].regular_price, data[i].description, data[i].image_ref, data[i].aisle, data[i].aisle_location, data[i].numOfItems, data[i].quantity);
 			list_of_products.push(temp);
 			console.log(list_of_products[i]);
 		}
-	productMatch(list_of_products,0);
+	productMatch(list_of_products);
 
-}*/
+}
 
-function productMatch(list_of_products,ID) {
-	var id = ID;
+function productMatch(list_of_products) {
+	var name = document.getElementsByTagName("body")[0].id;
 	for(let i = 0; i < list_of_products.length; i++)
-		if(list_of_products[i].id == id)
+		if(list_of_products[i].name == name)
 		{
 			var element = list_of_products[i];
 			console.log(element);
@@ -103,13 +67,12 @@ function productMatch(list_of_products,ID) {
 function content(element) {
 	var price = null;
 	element.regular_price = (Math.round(element.regular_price * 100) / 100).toFixed(2);
-	element.aisle_location = aisleLocation(element.aisle);
-	if(element.discount_price == null || isNaN(element.discount_price)){
+	if(element.discount_price == null){
 		price = element.regular_price;
 		document.getElementById("target_spot").innerHTML = 
 			'<div class="row">'+
 				'<div class="col-md-12 mt-4">'+
-					'<a class="prev-nav" href="index.php">Home</a><a> / </a><a class="prev-nav" href="'+element.aisle_location+'">' + element.aisle + '</a><a> / </a><a>'+element.name+'</a>'+
+					'<a class="prev-nav" href="index.html">Home</a><a> / </a><a class="prev-nav" href="'+element.aisle_location+'">' + element.aisle + '</a><a> / </a><a>'+element.name+'</a>'+
 				'</div>'+
 			'</div>'+
 			'<div class="row">'+
@@ -123,7 +86,7 @@ function content(element) {
 						'<p class="price-name mb-0">'+element.regular_price+'$</p>'+
 						'<p class="company-name-2">Quantity</p>'+
 						'<form action= "PHP FILE FOR LATER!">'+
-							'<input class="quantity-input-2 mr-1" type="number" id="quantity" name="'+element.id+'" min="1" max="99" value="" onload="calTotalPrice(value, '+element.discount_price+');" onchange="storeData(name, value); calTotalPrice(value, '+element.regular_price+');">'+
+							'<input class="quantity-input-2 mr-1" type="number" id="quantity" name="'+element.name+'" min="1" max="99" value="" onload="calTotalPrice(value, '+element.discount_price+');" onchange="storeData(name, value); calTotalPrice(value, '+element.regular_price+');">'+
 							'<button class="justify-content-center mt-0 pb-1" id="quantity_button" onclick="addToCart();" type="button">ADD TO CART</button>'+
 						'</form>'+
 						'<p class="price-name" id="total-price"><p>'+
@@ -142,7 +105,7 @@ function content(element) {
 		document.getElementById("target_spot").innerHTML = 
 			'<div class="row">'+
 				'<div class="col-md-12 mt-4">'+
-					'<a class="prev-nav" href="index.php">Home</a><a> / </a><a class="prev-nav" onclick="changeId();" href="'+element.aisle_location+'">' + element.aisle + '</a><a> / </a><a>'+element.name+'</a>'+
+					'<a class="prev-nav" href="index.html">Home</a><a> / </a><a class="prev-nav" onclick="changeId();" href="'+element.aisle_location+'">' + element.aisle + '</a><a> / </a><a>'+element.name+'</a>'+
 				'</div>'+
 			'</div>'+
 			'<div class="row">'+
@@ -157,7 +120,7 @@ function content(element) {
 						'<p class="regular-price-name mb-0">'+element.regular_price+'$</p>'+
 						'<p class="company-name-2">Quantity</p>'+
 						'<form action= "PHP FILE FOR LATER!">'+
-							'<input class="quantity-input-2 mr-1" type="number" id="quantity" name="'+element.id+'" min="1" max="99" value="" onload="calTotalPrice(value, '+element.discount_price+');" onchange="storeData(name, value); calTotalPrice(value, '+element.discount_price+');">'+
+							'<input class="quantity-input-2 mr-1" type="number" id="quantity" name="'+element.name+'" min="1" max="99" value="" onload="calTotalPrice(value, '+element.discount_price+');" onchange="storeData(name, value); calTotalPrice(value, '+element.discount_price+');">'+
 							'<button class="justify-content-center mt-0 pb-1" id="quantity_button" onclick="addToCart();" type="button">ADD TO CART</button>'+
 						'</form>'+
 						'<p class="price-name" id="total-price"><p>'+
@@ -221,17 +184,16 @@ function storeData(name, value)
 function loadPData()
 {
   { 
-  	var stored = document.getElementById("quantity").name;
-    document.getElementById("quantity").value = sessionStorage.getItem(stored);
+  	var body = document.getElementsByTagName("body")[0].id;
+    document.getElementById("quantity").value = sessionStorage.getItem(body);
   }
 }
 
 
 function addToCart(){
-    var stored = document.getElementById("quantity").name;
-    var item = document.getElementsByClassName("item-page-name")[0];
-    var desiredQuantity = sessionStorage.getItem("@"+stored);
-    var addedQuantity = sessionStorage.getItem(stored);
-    alert(addedQuantity + " " + item.textContent + " have been added to your cart!");
-	sessionStorage.setItem("@"+stored,Number(desiredQuantity) + Number(document.getElementById("quantity").value));    	
+    var body = document.getElementsByTagName("body")[0].id;
+    var desiredQuantity = sessionStorage.getItem("@"+body);
+    var addedQuantity = sessionStorage.getItem(body);
+    alert(addedQuantity + " " + body + " have been added to your cart!");
+	sessionStorage.setItem("@"+body,Number(desiredQuantity) + Number(document.getElementById("quantity").value));    	
 }
