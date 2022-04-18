e723ac58b19d60e1f2c7c63e7c544e54f373f78b
Gawain <60302096+DragonGawain@users.noreply.github.com>
2022-04-17 22:02:33 -0400

    Update allOrders.php

--- allOrders.php
+++ allOrders.php
@@ -1,66 +1,71 @@
+ <?php include "head.inc.php" ?>
 
-<?php include "head.inc.php" ?>
 <body class="backstore">
-	<?php include "header.html" ?>
 
-		<div class="container">
+ <?php include "header.html" ?>
+ <div class="container">
+	<script>
+	function contentGenerate(){
+		var tempOrders = document.getElementById("selectOrder").options[document.getElementById("selectOrder").selectedIndex].text;
+		var element = null;
+		for(var i = 0; i < orderArray.length; i++){
+			if(tempOrders == orderArray[i].email){
+				element = orderArray[i];
+			}
+		}
+		if(element == null){
+			document.getElementById("table1").innerHTML = "";
+		}
+		else {
+			document.getElementById("table1").innerHTML = '<tr>' +
+			'<th colspan=0> Order Specifics </th> <td> <input type="submit" name="update" value="Edit"></input> <input type="submit" name="delete" value="Delete"></input> </td>' +
+			'</tr>' +
+			'<tr>' +
+			'<th> Products and quantities <input type="text" name="product_quantities" value="' + element.product_quantities + '"></th>' +
+			'<th> Final price of order <input type="text" name="final_price" value="' + element.final_price  + '"></th>' +
+			'<th> Order ID <input type="text" name="ID" readonly value="' + element.id + '"></th>' +
+			'</tr>';
+		}
+	}
 
-      <script>
-        var jsvar;
-        var orderList = [];
-        
-			<?php
-  $xml = simplexml_load_file('OrderDataBase.xml') or die("cant load xml");
-      foreach ($xml as $record) {
-        if(!(empty($record->id)))
-          echo "jsvar = new order('$record->id','$record->product_quantities','$record->final_price');
-        orderList.push(jsvar);";
-  }
-      ?>  
-        </script>
-		</div>
-  
-      <!-- create order list table -->
-    <div class="orderList-container">
-      <div class="row justify-content-center">
-        <div class="col-lg-9">
-          <div class="table-responsive mt-2">
-           
-            <table class="table table-striped text-center" id = "allOrders-table">
-              <thead>
-                <tr>
-                  <td colspan = 3>
-                    <h2>All Orders</h2>
-                  </td>
-                </tr>
-                <tr>
-                  <th>Order ID</th>
-                  <th>Order items</th>
-                  <th>Final Price of Order</th>
-                 </tr>
-                </thead>
-              
-              <tbody>
-                
-                <script>
-                  for(var i = 0; i < orderList.length; i++) 
-                  {
-                    document.write("<tr>");
-                    document.write("<td>"+orderList[i]["id"]+"</td>");
-                    var temp = orderList[i]["product_quantities"];
-			temp = "<pre>" + temp.replace(/</g, "&lt;") + "</pre>"
-                    document.write("<td>"+temp+"</td>");
-                    document.write("<td>"+orderList[i]["final_price"]+"</td>");
-                    document.write("</tr>")  
-                  }
-                  
-                </script>
-              </tbody>
-            </div>
-          </div>
-        </div>
-      </div>
+	</script>
+
+ 	 Product List :
+ 				<select id="selectOrder">
+ 					<option value="">--- Select ---</option>
+ 				</select>
+ 					<button class="selectorButton" type="submit" name="Submit" value="Select" onclick="contentGenerate();"> Select </button>
+
+					<script>
+
+				 	 var orderArray = getOrder();
+				 		var selectOrderID = document.getElementById("selectOrder");
+				 		var string = '<option value="">--- Select ---</option>';
+				 		for(var i = 0; i < orderArray.length; i++){
+				 			string = string.concat('<option name="'+ orderArray[i].id + '" value=""> ' + orderArray[i].id + '</option>');
+				 		}
+				 		document.getElementById("selectOrder").innerHTML = string;
+
+				  </script>
+
+<form method="POST" action="deleteOrder.php"><table id="table1"></table></form>
+<form method="POST" action="addOrder.php">
+<table align='center' cellspacing=2 cellpadding=5 id="data_table" border=0>
+<tr>
+<th colspan=0>Add New Order</th> <td> <input class="selectorButton" type="submit" name="add" value="Add"></td>
+</tr>
+<tr>
+<th> Product Quantities <input type="text" name="product_quantities"></th>
+<th> Final Price <input type="text" name="final_price"></th>
+</tr>
+</form>
+
+</table>
+
+</div>
 
 </body>
 
 <?php include "footer.html" ?>
+
+</html>
